73f6f65d2ff5462849ac936a65475525899a72d0
Railly Hugo <railly_2009@hotmail.com>
2024-06-01 19:05:24 -0500

    fix: add experimental flag

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  experimental: {
+    optimizePackageImports: ["shiki", "@shikijs/monaco"],
+  },
 };
