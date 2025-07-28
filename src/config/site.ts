e89886fa87f299f2f43fa181a82100acb4a29518
Railly <railly_2009@hotmail.com>
2025-07-28 15:11:51 -0500

    feat: update layout and site configuration for enhanced metadata and theme support
    - Refactored the RootLayout component to utilize site configuration for dynamic metadata, including title, description, and Open Graph properties.
    - Added theme color meta tags for light and dark modes to improve user experience.
    - Introduced a new site configuration file to centralize site-related settings and metadata.

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "Tinte",
+  url: "https://tinte.dev",
+  ogImage: "https://tinte.dev/og.jpg",
+  description:
+    "Tinte is a multi-platform theme generator for your favorite tools.",
+  links: {
+    twitter: "https://twitter.com/raillyhugo",
+    github: "https://github.com/railly/tinte",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
